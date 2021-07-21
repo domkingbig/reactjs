107e7ad504ea2e47ea4e3de296d55407c53eda73
Huy Tu <huytukhtn@gmail.com>
2021-07-21 17:47:25 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pham Huy Tu
         </p>
         <a
           className="App-link"
